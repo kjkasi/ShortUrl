263fa0f7146676bf8bbe82f38a500373077023e6
root <localhost@localdomain>
2022-11-30 10:08:32 +0300

    No message

---
+++ ShortUrl.App/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShortUrl.App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortUrl.App.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetItemById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddItem(Token item)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ShortUrl.App/Models/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortUrl.App.Models
+{
+    public class Token
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+
+        public string ShortUr { get; set; }
+    }
+}
